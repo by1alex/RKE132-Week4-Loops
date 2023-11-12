e7b2984b49e27f0daaee94ac3d86ee6ddaec0fc9
Aleksandr Bychkov <Aleksandr.bychkov@tktk.ee>
2023-11-12 09:19:35 +0200

    first commit

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,17 @@
+﻿
+//FOR- loop
+
+int i = 0;
+
+for(i = 0; i < 5; i++)  //i ++ = i + 1
+{
+    Console.WriteLine("WE DON'T NEED NO EDUCATION");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day!");
+
+
+
+
+
---
+++ randomNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿Random rnd = new Random();
+
+int myRandomNum;
+
+int myRandomSum = 0;
+
+
+for (int i = 0; i < 10; i++)
+{
+    myRandomNum = rnd.Next(0, 11); //0 - 10
+    myRandomSum = myRandomSum + myRandomNum;
+    Console.WriteLine($"My random number is: {myRandomNum}");
+}
+
+Console.WriteLine($"Random sum total: {myRandomSum}");
